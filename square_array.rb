1f38086b4df6beae8862e26cdd166d5b947dac62
William Mittard <billymittard@ymail.com>
2020-04-15 13:56:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,7 @@
 def square_array(array)
-  array.each do |square|
-      puts #{array}** 2
-  end
-end
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
+  array = []
+  array.each { |square| arr << square ** 2 }
   arr
 end
 
-my_arr = [1, 2]
+my_arr = [1, 2,3]
